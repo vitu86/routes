363a7dc68290c9ff0650ce700384b46f3f2d7498
Vitor Costa <vitu86@gmail.com>
2019-02-26 23:39:47 -0300

    Added Mapkit and Photos View Controller

--- Routes/Routes/ViewControllers/MapViewController.swift
+++ Routes/Routes/ViewControllers/MapViewController.swift
@@ -7,18 +7,31 @@
 //
 
 import UIKit
+import MaterialComponents
 
 class MapViewController: UIViewController {
+    
+    // MARK: - IBOutlets
+    @IBOutlet weak var photosButton: MDCButton!
+    
+    // MARK: - Private properties
+    private var photosBTScheme:MDCButtonScheme!
     
     // MARK: - View Controller Override Functions
     override func viewDidLoad() {
         super.viewDidLoad()
-        configureNavigationBar()
+        configureUI()
     }
     
     // MARK: - Private Functions
-    private func configureNavigationBar() {
+    private func configureUI() {
+        // Configure Navigation Bar
         title = "Map"
         self.navigationController?.navigationBar.tintColor = UIColor(named: "BasePurple")
+        
+        // Configure Photos Button
+        photosBTScheme = MDCButtonScheme()
+        MDCContainedButtonThemer.applyScheme(photosBTScheme, to: photosButton)
+        photosButton.setBackgroundColor(UIColor(named: "BasePurple"))
     }
 }
---
+++ Routes/Routes/ViewControllers/PhotosViewController.swift
@@ -0,0 +1,16 @@
+//
+//  PhotosViewController.swift
+//  Routes
+//
+//  Created by Vitor Costa on 26/02/19.
+//  Copyright © 2019 Vitor Costa. All rights reserved.
+//
+
+import UIKit
+
+class PhotosViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Photos"
+    }
+}
