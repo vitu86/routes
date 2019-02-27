074c29416af7721e7b2f5d8b75d3b75db822075a
Vitor Costa <vitu86@gmail.com>
2019-02-27 18:51:19 -0300

    Updating UIViewController extension

--- Routes/Routes/Extensions/UIViewControllerExtensions.swift
+++ Routes/Routes/Extensions/UIViewControllerExtensions.swift
@@ -17,36 +17,40 @@ extension UIViewController {
     func showAlert(title:String, message:String, okFunction: returnAlertFunction? = nil, cancelFunction: returnAlertFunction? = nil) {
         let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
         if let cancelFunction = cancelFunction {
-            alert.addAction(UIAlertAction(title: "Cancelar", style: .default, handler: cancelFunction))
+            alert.addAction(UIAlertAction(title: "Cancel", style: .default, handler: cancelFunction))
         }
         alert.addAction(UIAlertAction(title: "OK", style: .default, handler: okFunction))
-        self.present(alert, animated: true)
+        present(alert, animated: true)
     }
     
     func showCenterIndicator() {
         UIApplication.shared.beginIgnoringInteractionEvents()
         let activityIndicator = ActivityIndicator.shared
-        activityIndicator.cycleColors = [UIColor.black]
-        activityIndicator.strokeWidth = 4
-        activityIndicator.radius = 24
         activityIndicator.startAnimating()
-        self.view.addSubview(activityIndicator)
+        view.addSubview(activityIndicator)
     }
     
     func hideCenterIndicator() {
         UIApplication.shared.endIgnoringInteractionEvents()
         let activityIndicator = ActivityIndicator.shared
         activityIndicator.stopAnimating()
+        activityIndicator.removeFromSuperview()
     }
     
 }
 
 class ActivityIndicator: MDCActivityIndicator {
+    
     static let shared = MDCActivityIndicator(frame: CGRect(x: UIScreen.main.bounds.width * 0.5 - 16, y:
         UIScreen.main.bounds.height * 0.5 - 16, width: 32, height: 32))
+    
     private init() {
         super.init(frame: CGRect.init())
+        cycleColors = [UIColor.black]
+        strokeWidth = 4
+        radius = 24
     }
+    
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
